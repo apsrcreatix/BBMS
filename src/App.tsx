4fcd051cbf3ef2455fbdf5da4b645da633e15f19
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-02-24 05:25:27 +0530

    rename LeftDrawer->Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import Skeleton from "./components/LeftDrawer";
+import Skeleton from "./components/Drawer";
 
 class App extends React.Component {
   public render() {
