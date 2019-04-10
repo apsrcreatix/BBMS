648d7c95212babd20261ac31242e3601df45ef83
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-04-11 00:38:04 +0530

    fix screening api bug

--- src/components/Inputs/Screening/index.tsx
+++ src/components/Inputs/Screening/index.tsx
@@ -2,25 +2,42 @@ import * as React from "react";
 import MaterialTable from "material-table";
 import Divider from "@material-ui/core/Divider";
 import Config from "../../../Config";
-import Data from "../../Data";
 import axios from "axios";
-import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import AddScreening from "./AddScreening";
+
 const username = Config.AUTH.username;
 const password = Config.AUTH.token;
 const base_url = Config.SERVER_URL;
-const session_url = base_url + Config.PATHS.inputBlood;
+const session_url = base_url + Config.PATHS.inputScreening;
 
 export default class InputsScreening extends React.Component {
+  constructor(props: any){
+    super(props);
+    axios
+    .get(session_url, {
+      auth: {
+        username,
+        password
+      }
+    })
+    .then((response: any) => {
+      this.setState({
+        screeningData: response.data.response.tests
+      });
+      console.log(this.state.screeningData+""+response.data.response.success);
+    })
+    .catch(function(error: any) {
+      console.log(`error in authentication : ${error}`);
+    });
+  }
   state = {
-    screeningData: [],
-    selectedScreening: ""
-  };
+    screeningData: []
+    };
 
   async fetchData() {
     await axios
-      .get(session_url + "?type=" + this.state.selectedScreening, {
+      .get(session_url, {
         auth: {
           username,
           password
@@ -28,8 +45,9 @@ export default class InputsScreening extends React.Component {
       })
       .then((response: any) => {
         this.setState({
-          bloodData: response.data.response.stocks
+          screeningData: response.data.response.tests
         });
+        console.log(this.state.screeningData+""+response.data.response.success);
       })
       .catch(function(error: any) {
         console.log(`error in authentication : ${error}`);
@@ -46,42 +64,18 @@ export default class InputsScreening extends React.Component {
             <AddScreening />
           </div>
           <Divider />
-          <p>Choose to view status of data, then apply for result.</p>
           <div className="input-container">
             <div className="input-inputs">
-              <TextField
-                className="inputs"
-                select
-                label="Screening"
-                value={this.state.selectedScreening}
-                onChange={this.handleChange("selectedScreening")}
-                SelectProps={{
-                  native: true
-                }}
-                InputLabelProps={{
-                  shrink: true
-                }}
-                required
-                helperText="Choose one before apply"
-                margin="normal"
-              >
-                <option value="">Select a type</option>
-                {Data.DONOR_ID.map((option: any) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </TextField>
+  <p>Please press refresh button to view latest uploaded data.</p>
             </div>
             <div className="input-buttons">
               <Button
                 className="inputs"
                 variant="contained"
                 color="default"
-                disabled={this.state.selectedScreening == ""}
                 onClick={() => this.fetchData()}
               >
-                Apply
+                Refresh
               </Button>
             </div>
           </div>
