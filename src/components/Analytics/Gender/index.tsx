79a856adf237d2c1bd39a1df7f7f40c855fa9f63
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-04-20 11:42:32 +0530

    add data from api

--- src/components/Analytics/Gender/index.tsx
+++ src/components/Analytics/Gender/index.tsx
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 import { Pie } from "react-chartjs-2";
+import axios from "axios";
+import Config from "../../../Config";
+
+const username = Config.AUTH.username;
+const password = Config.AUTH.token;
+const base_url = Config.SERVER_URL;
+const session_url = base_url + Config.PATHS.getAnalytics;
 
 class Gender extends Component {
         state = {
-            chartData: {
-                labels: ["Male", "Female"],
-                datasets: [{
-                    data: [73, 27],
-                    backgroundColor: ["#FF6384", "#36A2EB"],
-                    hoverBackgroundColor: ["#FF6384", "#36A2EB"]
-                }]
-            }
+            chartData: {}
         };
+        componentDidMount(){
+            axios.get(session_url, {
+                auth: {
+                  username,
+                  password
+                }
+              })
+              .then((response: any) => {
+                let pair = response.data.response.gender;  
+                let tags = []; 
+                let values = [];
+                for(var key in pair){
+                    tags.push(key);
+                    values.push(pair[key]);
+                    }
+                this.setState({
+                    chartData:  {
+                        labels: tags,
+                        datasets: [{
+                            data: values,
+                            backgroundColor: ["#FF6384", "#36A2EB"],
+                            hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+                        }]
+                    }
+                })                
+              }).catch(function(error: any) {
+                console.log(`error in authentication : ${error}`);
+              });
+        }
     render() {
         return (
             <div>
